e2c1e8d73567d8220cafdfec12179112fbb6c905
KJ Jackson <kjjackson619@yahoo.com>
2021-11-09 22:13:23 -0500

    finish round one

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,19 +9,34 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 // Create function
 var fight = function () {
+    window.alert("Welcome to Robot Gladiators!");
+    window.alert("The fight has begun!");
     // Subtract the value of 'playerAttack' fro the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
     enemyHealth = enemyHealth - playerAttack;
     // Log a resulting message to the console so we know that it worked.
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
+    // Check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
     // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
     playerHealth = playerHealth - enemyAttack;
     // Log a resulting message to the console so that we know that it worked.
     console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
-    window.alert("Welcome to Robot Gladiators!");
+    // Check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 // Execute function
 fight();
