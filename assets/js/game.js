2150e4be764e8206c6a9a6304d1733db2acd5b4e
KJ Jackson <kjjackson619@yahoo.com>
2021-11-09 21:39:32 -0500

    round one

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,27 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+// Create function
+var fight = function () {
+    // Subtract the value of 'playerAttack' fro the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so that we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+    window.alert("Welcome to Robot Gladiators!");
+};
+// Execute function
+fight();
