7fc47211645ee888104d20a1f5452e9cfc6da514
KJ Jackson <kjjackson619@yahoo.com>
2021-11-15 13:20:17 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -64,6 +64,31 @@ var fight = function (enemyName) {
     }
 };
 
+var startgame = function () {
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for (var i = 0; i < enemyNames.length; i++) {
+    }
+    endgame();
+}
+var endgame = function () {
+    window.alert("The game has now ended. Let's see how you did!");
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+}
+var playAgainConfirm = window.confirm("Would you like to play again?");
+if (playAgainConfirm) {
+    startgame();
+}
+else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
 for (var i = 0; i < enemyNames.length; i++) {
 
     if (playerHealth > 0) {
@@ -81,4 +106,6 @@ for (var i = 0; i < enemyNames.length; i++) {
         window.alert('You have lost your robot in battle! Game Over!');
         break;
     }
+    startgame()
 }
+startgame()
